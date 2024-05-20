a434393d671b6ebda25b5a79655810505c54bfc4
GM Brhylle <gmjheizon027@gmail.com>
2024-05-20 11:53:11 +0800

    added tailwindcss integration

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-blue-500 uppercase m-4 p-4 w-10 h-10 border border-sky-800">
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewComponent',
+    props: {
+     msg: String,   
+    }
+}
+</script>
