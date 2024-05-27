12e74c8bf143cb8302f0fe6033b0ba404e0b57eb
Ritchmond James S. Tajarros <rjstajarros@iskolarngbayan.pup.edu.ph>
2024-05-27 17:12:25 +0800

    remove app.vue style

--- src/App.vue
+++ src/App.vue
@@ -15,17 +15,4 @@ export default {
 };
 </script>
 
-<style>
-html {
-  background-color: #c0bade;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
