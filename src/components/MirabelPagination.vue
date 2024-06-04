68958b7a873c7af6dfff6142eb95660779819548
MirabelRobert <mirabel_robertleoni@plpasig.edu.ph>
2024-06-04 09:21:16 +0800

    Cleaned the Dark mode and its logic in the parts of the code

--- src/components/MirabelPagination.vue
+++ src/components/MirabelPagination.vue
@@ -6,14 +6,12 @@
         v-for="page in MBLtotalPages"
         :key="page"
         @click="MBLgoToPage(page)"
-        :class="MBLbuttonClass(page)"
+        :class="[
+          'MBLpx-4 MBLpy-2 MBLrounded',
+          MBLcurrentPage === page ? 'MBLbg-primary MBLtext-accent' : 'MBLbg-secondary MBLtext-text'
+        ]"
       >
         {{ page }}
-      </button>
-    </div>
-    <div class="MBLmt-4">
-      <button @click="MBLtoggleDarkMode" :class="MBLmodeButtonClass">
-        Toggle Dark Mode
       </button>
     </div>
   </div>
@@ -21,39 +19,16 @@
 
 <script>
 export default {
-  name: 'MBLApp',
+  name: 'PaginationComponent',
   data() {
     return {
       MBLtotalPages: 10,
-      MBLcurrentPage: 1,
-      MBLisDarkMode: false,
+      MBLcurrentPage: 1
     };
-  },
-  computed: {
-    MBLmodeButtonClass() {
-      return {
-        'MBLpx-4 MBLpy-2 MBLrounded MBLbg-primary MBLtext-accent': !this.MBLisDarkMode,
-        'MBLpx-4 MBLpy-2 MBLrounded MBLbg-dark-primary MBLtext-dark-accent': this.MBLisDarkMode,
-      };
-    },
-    MBLbuttonClass() {
-      return (page) => {
-        return {
-          'MBLpx-4 MBLpy-2 MBLrounded': true,
-          'MBLbg-secondary MBLtext-text': !this.MBLisDarkMode && this.MBLcurrentPage !== page,
-          'MBLbg-primary MBLtext-accent': !this.MBLisDarkMode && this.MBLcurrentPage === page,
-          'MBLbg-dark-secondary MBLtext-dark-text': this.MBLisDarkMode && this.MBLcurrentPage !== page,
-          'MBLbg-dark-primary MBLtext-dark-accent': this.MBLisDarkMode && this.MBLcurrentPage === page,
-        };
-      }
-    }
   },
   methods: {
     MBLhandlePageChange(page) {
       this.MBLcurrentPage = page;
-    },
-    MBLtoggleDarkMode() {
-      this.MBLisDarkMode = !this.MBLisDarkMode;
     },
     MBLgoToPage(page) {
       this.MBLhandlePageChange(page);
@@ -81,24 +56,12 @@ export default {
   color: #29045D;
 }
 
-.MBLbg-dark-primary {
-  background-color: #4B225D;
-}
-
-.MBLtext-dark-accent {
-  color: #C7A2FB;
-}
-
 .MBLflex {
   display: flex;
 }
 
-.MBLspace-x-2 {
-  margin-right: 8px;
-}
-
-.MBLmt-4 {
-  margin-top: 16px;
+.MBLspace-x-2 > :not(:first-child) {
+  margin-left: 8px;
 }
 
 .MBLpx-4 {
@@ -121,13 +84,5 @@ export default {
 
 .MBLtext-text {
   color: #160326;
-}
-
-.MBLbg-dark-secondary {
-  background-color: #020024;
-}
-
-.MBLtext-dark-text {
-  color: #ECD9FC;
 }
 </style>
