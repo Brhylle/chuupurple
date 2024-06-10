2f1eb6985d6161bf047a7d0bbc79a6d9ab4a5ab6
GM Brhylle <gmjheizon027@gmail.com>
2024-06-10 10:12:15 +0800

    removed unused plugin🧩

--- plugins/animations.js
+++ plugins/animations.js
@@ -2,10 +2,9 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import SplitType from 'split-type';
 import Lenis from '@studio-freight/lenis';
-import { DrawSVGPlugin } from 'gsap';
 
 
-gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
 export default {
   install(app) {
@@ -13,6 +12,5 @@ export default {
     app.config.globalProperties.$ScrollTrigger = ScrollTrigger;
     app.config.globalProperties.$SplitType = SplitType;
     app.config.globalProperties.$Lenis = Lenis;
-    app.config.globalProperties.$DrawSVGPlugin = DrawSVGPlugin;
   }
 };
